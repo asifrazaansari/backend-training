3f9ec282334bb12eb5193a6ab4af19a34d84c99d
asifrazaansari <asifraza0231@gmail.com>
2022-08-30 21:47:25 +0530

    assignment/try-catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,60 +2,82 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-  let userData = req.body;
-  let savedData = await userModel.create(userData);
-  res.send({ msg: savedData });
+  try {
+    let userData = req.body;
+    let savedData = await userModel.create(userData);
+    res.status(201).send({ msg: savedData });
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
 
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "plutonium",
-      organisation: "FUnctionUp",
-    },
-    "functionup-plutonium-secret-key"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(404).send({ status: false, msg: "username or the password is not corerct" });
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "plutonium",
+        organisation: "FUnctionUp",
+      },
+      "functionup-plutonium-secret-key"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(200).send({ status: true, data: token });
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 };
 
 const getUserData = async function (req, res) {
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  res.send({ status: true, data: userDetails });
+
+  try {
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+    res.status(200).send({ status: true, data: userDetails });
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 };
 
 const updateUser = async function (req, res) {
-  let userId = req.params.userId
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
-  res.send({ status: true, data: updatedUser });
+  try {
+    let userId = req.params.userId
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+    res.status(201).send({ status: true, data: updatedUser });
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 };
 
 const postMessage = async function (req, res) {
-  let message = req.body.message
-  let user = req.user
-  let updatedPosts =user.posts
-  updatedPosts.push(message)
-  let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: updatedPosts }, { new: true })
-  return res.send({ status: true, data: updatedUser })
+  try {
+    let message = req.body.message
+    let user = req.user
+    let updatedPosts = user.posts
+    updatedPosts.push(message)
+    let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: updatedPosts }, { new: true })
+    return res.status(201).send({ status: true, data: updatedUser })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 const deleteUser = async function (req, res) {
-  let userId = req.params.userId
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
-  res.send({ status: true, data: updatedUser });
+  try {
+    let userId = req.params.userId
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+    res.status(200).send({ status: true, data: updatedUser });
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,35 +1,48 @@
 const userModel = require("../models/userModel")
 const jwt = require("jsonwebtoken");
+const { rawListeners } = require("../models/userModel");
 
 const userExist = async function (req, res, next) {
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  if (!user) {
-    return res.send("No such user exists");
-  }
-  req.user = user
-  next()
-}
-
-
-const authenticate = function(req, res, next) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-  req.token = token
-  next()
-}
-
-
-const authorise = function(req, res, next) {
-  let decodedToken = jwt.verify(req.token, "functionup-plutonium-secret-key");
-  let userId = req.params.userId
-  if(!(decodedToken.userId === userId)){
-    return res.send({status: false, msg: "userId is not correct, param user and token user not matched"})
-  }
-  else {
-    req.authorise = authorise;
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).send("No such user exists");
+    }
+    req.user = user
     next()
+  } catch (error) {
+      return res.status(500).send(error.message)
+  }
+}
+
+
+const authenticate = function (req, res, next) {
+  try {
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+    req.token = token
+    next()
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+
+const authorise = function (req, res, next) {
+  try {
+    let decodedToken = jwt.verify(req.token, "functionup-plutonium-secret-key");
+    let userId = req.params.userId
+    if (!(decodedToken.userId === userId)) {
+      return res.status(403).send({ status: false, msg: "userId is not correct, param user and token user not matched" })
+    }
+    else {
+      req.authorise = authorise;
+      next()
+    }
+  } catch (error) {
+    return res.status(500).send(error.message)
   }
 }
 
