6a99b81d3c4526b7f1be5bb1d583137b8a0e5300
asifrazaansari <asifraza0231@gmail.com>
2022-09-01 18:08:08 +0530

    assignment/vaccination session

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -60,7 +60,7 @@ let getByPin = async function (req, res) {
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -78,8 +78,28 @@ let getOtp = async function (req, res) {
     }
 }
 
+let getVaccByDis = async function (req, res) {
+    try {
+        let districtId = req.query.districtId
+        let date = req.query.date
+        //console.log(`query params are: ${districtId} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`
+        }
+        let result = await axios(options)
+        //console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getVaccByDis = getVaccByDis
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const CowinController = require("../controllers/cowinController")
 
-
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 
 router.get("/cowin/states", CowinController.getStates)
@@ -14,7 +9,7 @@ router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
-
+router.get("/cowin/getVaccinationByDistrict", CowinController.getVaccByDis)
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
